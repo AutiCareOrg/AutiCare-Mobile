1ed6b17bcccaadbe16d0a9f61b5d68af8f2e423e
Pedro Oliveira <peoliveira0101@gmail.com>
2025-05-22 22:39:52 -0300

    Azazel berremoth

--- app/src/main/java/com/example/myapplication/network/RetrofitClient.java
+++ app/src/main/java/com/example/myapplication/network/RetrofitClient.java
@@ -1,5 +1,8 @@
 package com.example.myapplication.network;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -8,8 +11,15 @@ public class RetrofitClient {
 
     public static Retrofit getInstance() {
         if (retrofit == null) {
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .connectTimeout(60, TimeUnit.SECONDS)
+                    .readTimeout(120, TimeUnit.SECONDS)
+                    .writeTimeout(120, TimeUnit.SECONDS)
+                    .build();
+
             retrofit = new Retrofit.Builder()
-                    .baseUrl("http://192.168.0.52:8080/") // sua API local
+                    .baseUrl("http://10.0.2.2:8080/") // ou IP local ou ngrok, se preferir
+                    .client(okHttpClient)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
